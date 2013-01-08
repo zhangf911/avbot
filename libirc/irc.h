4ed4bad282b575fff33aaae3338ff71c75a75994
microcai <microcaicai@gmail.com>
2013-01-08 18:06:52 +0800

    retry more

--- libirc/irc.h
+++ libirc/irc.h
@@ -46,7 +46,7 @@ typedef boost::function<void(const IrcMsg& msg)> privmsg_cb;
 class IrcClient
 {
 public:
-    IrcClient(boost::asio::io_service &io_service,const std::string& user,const std::string& user_pwd="",const std::string& server="irc.freenode.net", const std::string& port="6667",const unsigned int max_retry_count=3);
+    IrcClient(boost::asio::io_service &io_service,const std::string& user,const std::string& user_pwd="",const std::string& server="irc.freenode.net", const std::string& port="6667",const unsigned int max_retry_count=1000);
     ~IrcClient();
 private:
     void handle_read_request(const boost::system::error_code& err, std::size_t readed);
