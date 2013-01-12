ce5ffcdeea6a8fc22402bdf658b36954cc2183a0
kiki <kiki.caicai@gmail.com>
2013-01-12 20:50:55 +0800

    process

--- process.hpp
+++ process.hpp
@@ -1,20 +1,27 @@
 
 #pragma once
 
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
 #include <boost/asio.hpp>
 #include <boost/filesystem.hpp>
+#ifndef _WIN32
+#include <unistd.h>
+#include <sys/wait.h>
+#endif
 
 namespace boost {
-class process;
+namespace process{
+typedef boost::asio::local::stream_protocol::socket	stream;
+
 #ifndef WIN32
 namespace posix{
 	// fork and connect stdin/stdout, and excute the routine
-	template<typename Hander>
-	class fork : public boost::asio::local::stream_protocol::socket  {
+	class fork : public boost::asio::local::stream_protocol::socket, boost::asio::local::stream_protocol  {
  		int   fd_;
 		pid_t pid_;
-		friend class process;
-		fork(boost::asio::io_service & io, Hander hander , bool redirectstderr = false)
+	public:
+		fork(boost::asio::io_service & io, boost::function<void ()> hander , bool redirectstderr = false)
 		  :boost::asio::local::stream_protocol::socket(io)
 		{
 	  		int fd[2];
@@ -24,31 +31,48 @@ namespace posix{
 				throw std::bad_alloc();  //FIXME, use other?
 			if (pid_ == 0 )
 			{
-				close(fd[0]);
-				dup2(fd[1], 0);
-				dup2(fd[1], 1);
+				::close(fd[0]);
+				::dup2(fd[1], 0);
+				::dup2(fd[1], 1);
 				if (redirectstderr)
 					dup2(fd[1], 2);
-				close(fd[1]);
+				::close(fd[1]);
 				hander();
 			}
 			else{
-				close(fd[1]);
-				fd_ = dup(fd_[0]);
-				close(fd[0]);
-				boost::asio::local::stream_protocol::socket::assign(AF_UNIX, fd_);
+				::close(fd[1]);
+				fd_ = dup(fd[0]);
+				::close(fd[0]);
+				
+				boost::asio::local::stream_protocol::socket* baseclass = ((boost::asio::local::stream_protocol::socket*)this);
+				assign(*this,fd_);
 			}
+		}
+		~fork(){
+			int status;
+			::waitpid(pid_,&status,0);
 		}
 	};
 }
 
-class process{
-public:
-	process(boost::asio::io_service & io,const boost::filesystem::path & exefile);
-	
-	
-};
-	
+static void  run_exec(const boost::filesystem::path & exefile, std::vector< std::string > argv)
+{
+	char * cargv[argv.size()+1];
+	for(int i = 0; i < argv.size() ; i ++)
+	{
+		cargv[i] = (char*)( argv[i].c_str());
+	}
+	::execv(exefile.c_str(),cargv);
+}
+
+boost::shared_ptr<boost::process::stream>  respawn_as_socket(boost::asio::io_service & io,const boost::filesystem::path & exefile, std::vector< std::string >  argv)
+{
+	boost::shared_ptr<boost::process::stream>	 ptr;
+	if(access(exefile.c_str(),X_OK)<0)
+		throw "execuable not found";
+	posix::fork * forker = new boost::process::posix::fork(io,boost::bind(& run_exec, exefile,argv));
+	ptr.reset(forker,boost::bind(&posix::fork::~fork,forker));
+}
 
 #else
 
@@ -56,10 +80,12 @@ namespace win32{
 	
 }
 
-class process{
+// not supported on windows
+boost::process::stream  respawn_as_socket(boost::asio::io_service & io,const boost::filesystem::path & exefile, ...)
+{
 	
-};
+}
 
 #endif
-	
+}
 }
