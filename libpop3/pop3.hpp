6244e7d78ef4e2b0707c0561659f4e990b6a0402
microcai <microcaicai@gmail.com>
2013-03-11 01:51:10 +0800

    misc update

--- libpop3/pop3.hpp
+++ libpop3/pop3.hpp
@@ -5,7 +5,11 @@
 class pop3{
 public:
 	pop3(boost::asio::io_service & _io_service);
-	operator()(const boost::system::error_code & errorcode);
+
+	operator()(const boost::system::error_code & errorcode, std::size_t length = 0)
+	{
+
+	}
 private:
 	boost::asio::io_service & io_service;
 };
