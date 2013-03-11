a09a8a5f91b312de37571012862dd8f2c61574dd
microcai <microcaicai@gmail.com>
2013-03-11 11:44:10 +0800

    add coro, http_download wrapper re-write as coro

--- libpop3/pop3.hpp
+++ libpop3/pop3.hpp
@@ -1,15 +1,20 @@
 
 
 #include <boost/asio.hpp>
+#include "boost/coro/coro.hpp"
+#include "boost/coro/yield.hpp"
 
 class pop3{
 public:
 	pop3(boost::asio::io_service & _io_service);
 
-	operator()(const boost::system::error_code & errorcode, std::size_t length = 0)
+	// connect to pop.qq.com
+	void operator()(const boost::system::error_code & errorcode, std::size_t length = 0)
 	{
 
 	}
 private:
 	boost::asio::io_service & io_service;
 };
+
+#include  "boost/coro/unyield.hpp"
