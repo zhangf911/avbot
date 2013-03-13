f5c140d20b5b0abeea4404c7c339ca57ca330e1e
microcai <microcaicai@gmail.com>
2013-03-13 20:56:05 +0800

    pop3: login done

--- libpop3/pop3.hpp
+++ libpop3/pop3.hpp
@@ -1,9 +1,11 @@
 
 #pragma once
 
+#include <boost/bind.hpp>
 #include <boost/function.hpp>
 #include <boost/asio.hpp>
 #include <boost/foreach.hpp>
+#include <boost/concept_check.hpp>
 #include "boost/coro/coro.hpp"
 #include "boost/coro/yield.hpp"
 
@@ -12,13 +14,15 @@
 
 class pop3 : boost::coro::coroutine {
 public:
+	typedef void result_type;
+public:	
 	pop3(boost::asio::io_service & _io_service, std::string user, std::string passwd)
-		:io_service(_io_service), m_socket(new boost::asio::ip::tcp::socket(io_service)),
-		i(0), m_user(user), m_passwd(passwd), hosts( new std::vector<boost::asio::ip::tcp::endpoint>() )
+		:io_service(_io_service),
+		m_user(user), m_passwd(passwd),
+		hosts( new std::vector<boost::asio::ip::tcp::endpoint>() )
 	{
 		io_service.post(*this);
 	}
-
 	// connect to pop.qq.com
 	void operator()(const boost::system::error_code & ec = boost::system::error_code(), std::size_t length = 0)
 	{
@@ -28,6 +32,9 @@ public:
 		std::istream	inbuffer(m_streambuf.get());
 		reenter(this)
 		{
+restart:
+			m_socket.reset();
+
 			do{
 				hosts->clear();
 				// 延时 100ms
@@ -37,7 +44,7 @@ public:
 
 				// 失败了延时 10s
 				if(ec)
-					_yield ::boost::delayedcallsec(io_service,10, *this);
+					_yield ::boost::delayedcallsec(io_service,10, boost::bind(*this, ec, 0));
 			}while(ec);// dns解析到成功为止!
 
 			i = 0;
@@ -45,33 +52,48 @@ public:
 			do{
 				// 一个一个尝试链接.
 				endpoint = (*hosts)[i++];
+				m_socket.reset(new boost::asio::ip::tcp::socket(io_service));
 				_yield m_socket->async_connect(endpoint, *this);
 			}while(ec && i < hosts->size());
 
 			// 没连接上？　重试不　？
 			if(ec){
-				pop3 p(io_service, m_user, m_passwd);
-				return;
+				goto restart;
 			}
 			// 好了，连接上了.
-			
+			m_streambuf.reset(new boost::asio::streambuf);			
 			// "+OK QQMail POP3 Server v1.0 Service Ready(QQMail v2.0)"
 			_yield	boost::asio::async_read_until(*m_socket,*m_streambuf,"\n",*this);
 			inbuffer >> line;
+			if(line != "+OK"){
+				// 失败，重试.
+				goto restart;
+			}
 
 			// 发送用户名.
 			_yield m_socket->async_write_some(boost::asio::buffer(std::string("user ")+ m_user +"\n"), *this);
-
 			// 接受返回状态.
+			m_streambuf.reset(new boost::asio::streambuf);
 			_yield	boost::asio::async_read_until(*m_socket,*m_streambuf,"\n",*this);
+			inbuffer >> line;
+
 			// 解析是不是　OK.
+			if(line != "+OK"){
+				// 失败，重试.
+				goto restart;
+			}
 
 			// 发送密码.
 			_yield m_socket->async_write_some(boost::asio::buffer(std::string("pass ")+ m_passwd+"\n"), *this);
-
 			// 接受返回状态.
+			m_streambuf.reset(new boost::asio::streambuf);
 			_yield	boost::asio::async_read_until(*m_socket,*m_streambuf,"\n",*this);
+			inbuffer >> line;
 			// 解析是不是　OK.
+			if(line != "+OK"){
+				// 失败，重试.
+				goto restart;
+			}
 
 			// 完成登录. 开始接收邮件.
 			
