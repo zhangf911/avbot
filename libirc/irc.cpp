13e884287fe1df36e3c0fe924243976ba479f273
microcai <microcaicai@gmail.com>
2013-03-26 19:01:23 +0800

    style

--- libirc/irc.cpp
+++ libirc/irc.cpp
@@ -213,7 +213,6 @@ void IrcClient::handle_write_request(const boost::system::error_code& err, std::
 					boost::asio::async_write(socket_,
 						boost::asio::buffer(line),
 						boost::bind(&IrcClient::handle_write_request, this, _1, _2, boost::coro::coroutine())
-					
 					);
 				}else{
 					boost::delayedcallms(io_service, 5, boost::bind(&IrcClient::handle_write_request, this, boost::system::error_code(), 0, boost::coro::coroutine()));
