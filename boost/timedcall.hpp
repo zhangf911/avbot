ebced842f65892b5eb730112afc6bfb120e68a84
microcai <microcaicai@gmail.com>
2013-03-27 11:51:02 +0800

    delayedccall 少用一个成员变量

--- boost/timedcall.hpp
+++ boost/timedcall.hpp
@@ -4,28 +4,29 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/function.hpp>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 namespace boost {
 
 template<class timeunit>
 class base_delayedcall{
 public:
-	template<typename handler>
-	base_delayedcall(boost::asio::io_service &_io_service, int timeunitcount, BOOST_ASIO_MOVE_ARG(handler) _cb)
+	typedef void result_type;
+	template<class handler>
+	base_delayedcall(boost::asio::io_service &_io_service, int timeunitcount, handler _cb)
 		:io_service(_io_service),timer( new boost::asio::deadline_timer(io_service, timeunit(timeunitcount)))
 	{
 	    BOOST_ASIO_WAIT_HANDLER_CHECK(handler, _cb) type_check;
-		cb = _cb;
-		timer->async_wait(*this);
+ 		timer->async_wait(boost::bind(*this, boost::function<void()>(_cb), _1));
 	}
-	void operator()(const boost::system::error_code& error)
+	template<class handler>
+	void operator()(handler _cb, const boost::system::error_code& error)
 	{
-		cb();
+		io_service.post(_cb);
 	}
 private:
 	boost::asio::io_service &io_service;
 	boost::shared_ptr<boost::asio::deadline_timer> timer;
-	boost::function< void () > cb;
 };
 
 typedef base_delayedcall<boost::posix_time::seconds> delayedcallsec;
