50ea8ff233b30ec6da496db6dfafeab0e2b20d0b
microcai <microcaicai@gmail.com>
2013-03-28 17:22:17 +0800

    smtp stub

--- libmailexchange/smtp.cpp
+++ libmailexchange/smtp.cpp
@@ -1,2 +1,12 @@
 
+#include <boost/asio.hpp>
+
 #include "smtp.hpp"
+
+smtp::smtp(boost::asio::io_service& _io_service, std::string user, std::string passwd, std::string _mailserver)
+	:io_service(_io_service),
+	m_mailaddr(user), m_passwd(passwd),
+	m_mailserver(_mailserver)
+{
+
+}
--- libmailexchange/smtp.hpp
+++ libmailexchange/smtp.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <string>
+#include <boost/asio/io_service.hpp>
+
 class smtp{
-	
+	boost::asio::io_service & io_service;
+	std::string m_mailaddr,m_passwd,m_mailserver;
+
+public:
+	smtp(::boost::asio::io_service & _io_service, std::string user, std::string passwd, std::string _mailserver="");
 };
 
