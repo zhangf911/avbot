0c02fcc026153aec2d32a2e5dbcd49f69906fa7e
microcai <microcaicai@gmail.com>
2013-04-10 21:03:15 +0800

    fix compile error

--- consolestr.hpp
+++ consolestr.hpp
@@ -12,6 +12,19 @@ inline std::string ansi_utf8( std::string const &source, const std::string &char
 	return boost::locale::conv::between( source, "UTF-8", characters ).c_str();
 }
 
+// convert wide string for console output aka native encoding
+// because mixing std::wcout and std::cout is broken on windows
+inline std::string console_out_str(std::wstring in )
+{
+#ifdef WIN32
+	std::vector<char> szBuf(in.length()*2);
+	WideCharToMultiByte(CP_ACP, 0, in.c_str(), -1, &szBuf[0], szBuf.capacity(), NULL, NULL);
+	return std::string(szBuf.data());
+#else
+	return wide_utf8(in);
+#endif
+}
+
 // convert utf8 for console output aka native encoding
 // for linux ,  it does nothing (linux console always use utf8)
 // for windows, it convert to CP_ACP
@@ -24,13 +37,3 @@ inline std::string console_out_str(std::string in)
 #endif
 }
 
-inline std::string console_out_str(std::wstring in )
-{
-#ifdef WIN32
-	std::vector<char> szBuf(in.length()*2);
-	WideCharToMultiByte(CP_ACP, 0, in.c_str(), -1, &szBuf[0], szBuf.capacity(), NULL, NULL);
-	return std::string(szBuf.data());
-#else
-	return wide_utf8(in);
-#endif
-}
