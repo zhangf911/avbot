50ea53e45ee5aef17a5656bc442d9ab58c747308
海盗 <lovey599@gmail.com>
2013-04-12 12:55:30 +0800

    添加好了manifset支持

--- main.cpp
+++ main.cpp
@@ -36,7 +36,12 @@ namespace po = boost::program_options;
 
 #include <windows.h>
 #include <tchar.h>
+#include <CommCtrl.h> 
 #include "resource.h"
+
+#pragma comment(lib, "Comctl32.lib")
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #ifndef WINVER
 #define WINVER 0x0501
