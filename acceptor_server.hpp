cbc19a5a04e2ca1cbe86c57944d401fe0e7ffd05
microcai <microcaicai@gmail.com>
2013-04-13 02:56:26 +0800

    fix

--- acceptor_server.hpp
+++ acceptor_server.hpp
@@ -12,7 +12,7 @@
 
 namespace boost{
 
-template<typename ProtocolProcesser, typename Protocol = typename ProtocolProcesser::Protocol>
+template<class ProtocolProcesser, typename Protocol = typename ProtocolProcesser::Protocol>
 class acceptor_server :  boost::coro::coroutine
 {
 	boost::asio::io_service & io_service;
@@ -26,13 +26,13 @@ class acceptor_server :  boost::coro::coroutine
 public:
 	typedef typename Protocol::endpoint endpoint_type;
 
-	explicit acceptor_server(boost::asio::io_service & io, const endpoint_type & endpoint)
+	acceptor_server(boost::asio::io_service & io, const endpoint_type & endpoint)
 		:io_service(io), m_acceptor_socket(new boost::asio::basic_socket_acceptor<Protocol>(io, endpoint))
 	{
 		start_accept();
 	}
 
-	explicit acceptor_server(boost::shared_ptr<boost::asio::basic_socket_acceptor<Protocol> > acceptor_socket)
+	acceptor_server(boost::shared_ptr<boost::asio::basic_socket_acceptor<Protocol> > acceptor_socket)
 		:io_service(acceptor_socket->get_io_service()), m_acceptor_socket(acceptor_socket)
 	{
 		start_accept();
@@ -40,14 +40,16 @@ public:
 
 	void operator()(const boost::system::error_code & ec)
 	{
-		reenter(this)
+		CORO_REENTER(this)
 		{
 			do{
 				m_client_socket.reset(new boost::asio::basic_stream_socket<Protocol>(io_service));
 				_yield m_acceptor_socket->async_accept(*m_client_socket, *this);
-				_fork acceptor_server<ProtocolProcesser, Protocol>(*this)(ec);
+				if (!ec)
+					_fork acceptor_server<ProtocolProcesser, Protocol>(*this)(ec);
 			}while (is_parent());
-			ProtocolProcesser(*m_client_socket);
+			std::cout <<  "child here" << std::endl;
+			{ProtocolProcesser p(m_client_socket);}
 		}
 	}
 };
