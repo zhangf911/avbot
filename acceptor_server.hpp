ae79593d8ef0f721702e5eaa32a4a72d99b0fde6
microcai <microcaicai@gmail.com>
2013-04-16 21:14:28 +0800

    move impl to libavbot aka class avbot

--- acceptor_server.hpp
+++ acceptor_server.hpp
@@ -11,14 +11,15 @@
 #include "boost/coro/yield.hpp"
 
 namespace boost{
+namespace detail{
 
-template<class ProtocolProcesser, typename Protocol = typename ProtocolProcesser::Protocol>
-class acceptor_server :  boost::coro::coroutine
+template<typename Protocol, typename ProtocolProcesser>
+class acceptor_server_op :  boost::coro::coroutine
 {
 	boost::asio::io_service & io_service;
 	boost::shared_ptr<boost::asio::basic_socket_acceptor<Protocol> > m_acceptor_socket;
 	boost::shared_ptr<boost::asio::basic_stream_socket<Protocol> > m_client_socket;
-
+	ProtocolProcesser protocolprocesser;
 	void start_accept(){
 		io_service.post(boost::asio::detail::bind_handler(*this, boost::system::error_code()));
 	}
@@ -26,17 +27,18 @@ class acceptor_server :  boost::coro::coroutine
 public:
 	typedef typename Protocol::endpoint endpoint_type;
 
-	acceptor_server(boost::asio::io_service & io, const endpoint_type & endpoint)
-		:io_service(io), m_acceptor_socket(new boost::asio::basic_socket_acceptor<Protocol>(io, endpoint))
+	acceptor_server_op(boost::asio::io_service & io, const endpoint_type & endpoint, ProtocolProcesser protocolprocesser_)
+		:io_service(io), m_acceptor_socket(new boost::asio::basic_socket_acceptor<Protocol>(io, endpoint)), protocolprocesser(protocolprocesser_)
 	{
 		start_accept();
 	}
 
-	acceptor_server(boost::shared_ptr<boost::asio::basic_socket_acceptor<Protocol> > acceptor_socket)
-		:io_service(acceptor_socket->get_io_service()), m_acceptor_socket(acceptor_socket)
+	acceptor_server_op(boost::shared_ptr<boost::asio::basic_socket_acceptor<Protocol> > acceptor_socket, ProtocolProcesser protocolprocesser_)
+		:io_service(acceptor_socket->get_io_service()), m_acceptor_socket(acceptor_socket), protocolprocesser(protocolprocesser_)
 	{
 		start_accept();
 	}
+
 
 	void operator()(const boost::system::error_code & ec)
 	{
@@ -46,12 +48,21 @@ public:
 				m_client_socket.reset(new boost::asio::basic_stream_socket<Protocol>(io_service));
 				_yield m_acceptor_socket->async_accept(*m_client_socket, *this);
 				if (!ec)
-					_fork acceptor_server<ProtocolProcesser, Protocol>(*this)(ec);
+					_fork acceptor_server_op<Protocol, ProtocolProcesser>(*this)(ec);
 			}while (is_parent());
-			{ProtocolProcesser p(m_client_socket);}
+			protocolprocesser(m_client_socket);
 		}
 	}
+
 };
+
+}
+
+template<typename Protocol, class ProtocolProcesser>
+void acceptor_server(boost::asio::io_service & io, const boost::asio::ip::basic_endpoint<Protocol> & endpoint, ProtocolProcesser protocolprocesser)
+{
+	detail::acceptor_server_op<Protocol, ProtocolProcesser>(io, endpoint, protocolprocesser);
+}
 
 }
 
