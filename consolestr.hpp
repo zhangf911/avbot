88808dc5e8bc26697180ae89fcabd65808b53821
microcai <microcaicai@gmail.com>
2013-04-24 16:19:20 +0800

    这样的fix 可以么？

--- consolestr.hpp
+++ consolestr.hpp
@@ -13,25 +13,28 @@
 
 inline std::wstring ansi_wide( std::string const &source )
 {
-#ifdef WIN32
+#ifdef _WIN32
 	std::vector<wchar_t> szBuf(source.length()*2);
-	MultiByteToWideChar(CP_ACP, 0, source.c_str(), -1, &szBuf[0], szBuf.capacity());
+	MultiByteToWideChar(CP_ACP, 0, source.c_str(), source.length(), &szBuf[0], szBuf.capacity());
 	return std::wstring(szBuf.data());
 #else
 	return utf8_wide(source);
 #endif
 }
 
-inline std::string ansi_utf8( std::string const &source, const std::string &characters = "GB18030" )
+inline std::string console_utf8( std::string const &source)
 {
-	return wide_utf8(ansi_wide(source));
+#ifdef _WIN32
+	return wide_utf8(ansi_wide(source).c_str()).c_str();
+# endif
+	return source;
 }
 
 // convert wide string for console output aka native encoding
 // because mixing std::wcout and std::cout is broken on windows
 inline std::string console_out_str(std::wstring in )
 {
-#ifdef WIN32
+#ifdef _WIN32
 	std::vector<char> szBuf(in.length()*2);
 	WideCharToMultiByte(CP_ACP, 0, in.c_str(), -1, &szBuf[0], szBuf.capacity(), NULL, NULL);
 	return std::string(szBuf.data());
@@ -45,7 +48,7 @@ inline std::string console_out_str(std::wstring in )
 // for windows, it convert to CP_ACP
 inline std::string console_out_str(std::string in)
 {
-#ifdef WIN32
+#ifdef _WIN32
 	return console_out_str(utf8_wide(in.c_str()).c_str());
 #else
 	return in;
