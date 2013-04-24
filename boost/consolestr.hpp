149db57720cde29d79a0d866a85427f2d9c8ab92
microcai <microcaicai@gmail.com>
2013-04-24 16:59:19 +0800

    老实的使用 ansi_utf8 ?

--- boost/consolestr.hpp
+++ boost/consolestr.hpp
@@ -11,6 +11,12 @@
 # include <windows.h>
 #endif
 
+inline std::string ansi_utf8( std::string const &source, const std::string &characters = "GB18030" )
+{
+	return boost::locale::conv::between( source, "UTF-8", characters ).c_str();
+}
+
+
 inline std::wstring ansi_wide( std::string const &source )
 {
 #ifdef _WIN32
@@ -20,14 +26,6 @@ inline std::wstring ansi_wide( std::string const &source )
 #else
 	return utf8_wide(source);
 #endif
-}
-
-inline std::string console_utf8( std::string const &source)
-{
-#ifdef _WIN32
-	return wide_utf8(ansi_wide(source).c_str()).c_str();
-# endif
-	return source;
 }
 
 // convert wide string for console output aka native encoding
@@ -43,13 +41,19 @@ inline std::string console_out_str(std::wstring in )
 #endif
 }
 
+inline std::string utf8_ansi( std::string const &source, const std::string &characters = "GB18030" )
+{
+	return boost::locale::conv::between( source, characters, "UTF-8" ).c_str();
+}
+
+
 // convert utf8 for console output aka native encoding
 // for linux ,  it does nothing (linux console always use utf8)
 // for windows, it convert to CP_ACP
 inline std::string console_out_str(const std::string & in)
 {
 #ifdef _WIN32
-	return console_out_str(utf8_wide(in.c_str()).c_str());
+	return utf8_ansi(in);
 #else
 	return in;
 #endif
