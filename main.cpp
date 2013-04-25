05f96f6d2f700fc2d7344642418a525222dad3fe
microcai <microcaicai@gmail.com>
2013-04-25 09:46:55 +0800

    default to run rpc

--- main.cpp
+++ main.cpp
@@ -251,7 +251,7 @@ int main( int argc, char *argv[] )
 	std::string chanelmap;
 	std::string mailaddr, mailpasswd, pop3server, smtpserver;
 
-	uint rpcport = 6176;
+	uint rpcport;
 
 	boost::asio::io_service io_service;
 
@@ -283,9 +283,9 @@ int main( int argc, char *argv[] )
 	( "mailpasswd",	po::value<std::string>( &mailpasswd ), 	"password of mail" )
 	( "pop3server",	po::value<std::string>( &pop3server ), 	"pop server of mail,  default to pop.[domain]" )
 	( "smtpserver",	po::value<std::string>( &smtpserver ), 	"smtp server of mail,  default to smtp.[domain]" )
-	( "rpcport",	po::value<uint>(&rpcport), 				"run rpc server on port 6176")
 
 	( "localimage", po::value<bool>( &(mybot.fetch_img) )->default_value(true),	"fetch qq image to local disk and store it there")
+	( "rpcport",	po::value<uint>(&rpcport)->default_value(6176),				"run rpc server on port 6176")
 
 	( "preambleqq",		po::value<std::string>( &preamble_qq_fmt )->default_value( console_out_str("qq(%a) :") ),
 		console_out_str("为QQ设置的发言前缀, 默认是 qq(%a):").c_str() )
@@ -523,7 +523,7 @@ int main( int argc, char *argv[] )
 #endif
 	}
 
-	if (vm.count("rpcport") && rpcport > 0)
+	if (rpcport > 0)
 	{
 		// 调用 acceptor_server 跑 avbot_rpc_server 。 在端口 6176 上跑哦!
  		boost::acceptor_server(io_service,
