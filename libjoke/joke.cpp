d3f5084c6843bcd34ae1fd72480e5837a87bf5bd
microcai <microcaicai@gmail.com>
2013-04-29 17:02:59 +0800

    add ericsimith  as copyright holder

--- libjoke/joke.cpp
+++ libjoke/joke.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013  microcai <microcai@fedoraproject.org>
+ * Copyright (C) 2013  ericsimith <ericsimith@hotmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
