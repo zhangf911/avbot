9458927faf20d1497e1fc02bfe5cd27f713a2484
microcai <microcaicai@gmail.com>
2013-05-04 14:06:09 +0800

    move urlpreview to seperated file

--- extension/luascript.cpp
+++ extension/luascript.cpp
@@ -3,6 +3,11 @@
 
 #include "luascript.hpp"
 
+callluascript::callluascript( boost::asio::io_service &_io_service,  boost::function<void ( std::string ) >  sender, std::string channel_name )
+	: io_service( _io_service ), m_sender( sender ), m_channel_name( channel_name )
+{
+}
+
 void callluascript::operator()( boost::property_tree::ptree message ) const
 {
 	if( message.get<std::string>( "channel" ) != m_channel_name )
@@ -11,4 +16,5 @@ void callluascript::operator()( boost::property_tree::ptree message ) const
 	}
 
 	// 准备调用 LUA 脚本.
+
 }
--- extension/luascript.hpp
+++ extension/luascript.hpp
@@ -10,18 +10,17 @@
 #include <boost/asio.hpp>
 #include <boost/property_tree/ptree.hpp>
 
+struct lua_State;
+
 class callluascript
 {
 	boost::asio::io_service &io_service;
 	boost::function<void ( std::string ) > m_sender;
 	std::string m_channel_name;
+	lua_State * lua_State;
 
 public:
-	template<class MsgSender>
-	callluascript( boost::asio::io_service &_io_service,  MsgSender sender, std::string channel_name )
-		: io_service( _io_service ), m_sender( sender ), m_channel_name( channel_name )
-	{
-	}
+	callluascript( boost::asio::io_service &_io_service,  boost::function<void ( std::string ) >  sender, std::string channel_name );
 
 	// on_message 回调.
 	void operator()( boost::property_tree::ptree message ) const;
