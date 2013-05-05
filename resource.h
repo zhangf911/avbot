291563d47228bf0e97fde6cdc3039da108a12867
cai <microcaicai@gmail.com>
2013-05-05 08:26:27 +0800

    optional irc settings

--- resource.h
+++ resource.h
@@ -5,7 +5,6 @@
 #define IDD_AVSETTINGS_DIALOG           101
 #define IDI_ICON1                       102
 #define IDI_AVBOT_ICON                  102
-
 #define IDC_EDIT_USER_NAME              1001
 #define IDC_EDIT_PWD                    1002
 #define IDC_EDIT_IRC_CHANNEL            1003
@@ -15,6 +14,7 @@
 #define IDC_EDIT_XMPP_NICK              1007
 #define IDC_EDIT_XMPP_PWD               1008
 #define IDC_CHECK_XMPP                  1009
+#define IDC_CHECK_IRC                   1010
 
 // Next default values for new objects
 // 
