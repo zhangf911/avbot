6797a350d0afc76d156c5257bbbe0a17e5be7442
microcai <microcaicai@gmail.com>
2013-05-07 02:15:10 +0800

    fix clang build

--- extension/luabind/luabind/detail/format_signature.hpp
+++ extension/luabind/luabind/detail/format_signature.hpp
@@ -14,11 +14,19 @@
 # include <boost/mpl/size.hpp>
 
 namespace luabind {
+namespace adl
+{
 
 class object;
 class argument;
 template <class Base>
 struct table;
+
+} // namespace adl
+
+using adl::object;
+using adl::argument;
+using adl::table;
 
 } // namespace luabind
 
