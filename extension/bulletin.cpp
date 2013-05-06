115b3c48d661e66e572fbac81eed3d5db1a6a305
microcai <microcaicai@gmail.com>
2013-05-07 01:06:22 +0800

    bulletin stash

--- extension/bulletin.cpp
+++ extension/bulletin.cpp
@@ -1,8 +1,34 @@
 
 #include "bulletin.hpp"
 
+void bulletin::load_settings()
+{
+	boost::filesystem::path settingsfile = boost::filesystem::current_path() / m_channel_name / "bulletin_setting" ;
+	if (boost::filesystem::exists(settingsfile))
+	{
+		std::ifstream bulletin_setting(settingsfile.string().c_str());
+
+		std::string line;
+		std::getline(bulletin_setting, line);
+
+		m_settings->push_back(line);
+	}
+}
+
+// 在这里调度一下下一次启动.
+void bulletin::schedule_next() const
+{
+	// 获取当前时间,  以 分为单位步进
+	// 直到获得一个能满足要求的时间值
+	// 或则超过 24h
+	// 如果这样就直接设定一天以后再重新跑
+	// 避免 cpu 无限下去
+	
+
+}
+
 void bulletin::operator()( boost::property_tree::ptree message ) const
 {
-	
+	// 其实主要是为了响应 .qqbot bulletin 命令.
 
 }
--- extension/bulletin.hpp
+++ extension/bulletin.hpp
@@ -9,16 +9,21 @@
 #include <boost/function.hpp>
 #include <boost/asio.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <boost/filesystem.hpp>
+
 #include <avhttp.hpp>
 
 #include "extension.hpp"
 
 class bulletin : avbotextension
 {
+	boost::shared_ptr<std::vector<std::string> > m_settings;
+	void load_settings();
+	void schedule_next() const;
 public:
 	template<class MsgSender>
 	bulletin( boost::asio::io_service &_io_service,  MsgSender sender, std::string channel_name )
-	  : avbotextension(_io_service, sender, channel_name)
+	  : avbotextension(_io_service, sender, channel_name), m_settings(new std::vector<std::string>)
 	{
 		// 读取公告配置.
 		// 公告配置分两个部分, 一个是公告文件  $qqlog/$channel_name/bulletin.txt
@@ -28,8 +33,14 @@ public:
 		// 时间是 YY-MM-DD-HH-MM 这个的格式
 		// 留 * 表示 每
 		// 比如 *-*-*-08-00 表示每天早上 8 点
-		
+		// 保存在  $qqlog/$channel_name/bulletin_setting
 
+		// 那么, 读取公告设置吧!
+		try{
+			load_settings();
+		}catch (std::runtime_error){}
+
+		schedule_next();
 	}
 
 	// on_message 回调.
