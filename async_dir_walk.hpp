d6a8c6c6c272911e16c17913eec28206f4b3bff4
microcai <microcaicai@gmail.com>
2013-05-09 00:34:57 +0800

    更新 async_image_checker.hpp

--- async_dir_walk.hpp
+++ async_dir_walk.hpp
@@ -26,10 +26,10 @@ public:
 	async_dir_walk( boost::asio::io_service & _io_service, boost::filesystem::path path, DirWalkHandler _dir_walk_handler, CompleteHandler _complete_handler)
 		: io_service( _io_service ), dir_it_cur(path), dir_walk_handler(_dir_walk_handler), complete_handler(_complete_handler)
 	{
-		io_service.post( boost::asio::detail::bind_handler( *this, boost::coro::coroutine() ) );
+		avloop_idle_post(io_service, boost::asio::detail::bind_handler( *this, boost::coro::coroutine(), boost::system::error_code() ) );
 	}
 
-	void operator()( boost::coro::coroutine coro )
+	void operator()( boost::coro::coroutine coro, boost::system::error_code ec )
 	{
 		// 好了，每次回调检查一个文件，这样才好，对吧.
 		reenter( &coro )
@@ -37,12 +37,12 @@ public:
 			for( ; dir_it_cur != dir_it_end ; dir_it_cur++ )
 			{
 				// 好，处理 dir_it_cur dir_it_;
-				_yield dir_walk_handler(dir_it_cur->path(), io_service.wrap(boost::bind( *this, coro )) );
+				_yield dir_walk_handler(dir_it_cur->path(), io_service.wrap(boost::bind( *this, coro , _1)) );
 
-				_yield avloop_idle_post(io_service, boost::bind( *this, coro ) );
+				_yield avloop_idle_post(io_service, boost::bind( *this, coro, ec ) );
 			}
 
-			avloop_idle_post(io_service, complete_handler);
+			avloop_idle_post(io_service, boost::asio::detail::bind_handler(complete_handler, ec));
 		}
 	}
 
@@ -50,7 +50,7 @@ public:
 
 class DirWalkDumyHandler{
 public:
-	void operator()(){}
+	void operator()(boost::system::error_code ec){}
 };
 
 template<class DirWalkHandler, class CompleteHandler>
