85ad0e2e22088fdca8c44de4346a696234bfef50
microcai <microcaicai@gmail.com>
2013-05-09 00:19:12 +0800

    稍微修改一下, 使其支持完成回调

--- boost/async_dir_walk.hpp
+++ boost/async_dir_walk.hpp
@@ -12,18 +12,19 @@
 namespace boost{
 namespace detail{
 
-template<class DirWalkHandler>
+template<class DirWalkHandler, class CompleteHandler>
 class async_dir_walk
 {
 	boost::asio::io_service &io_service;
 	boost::filesystem::directory_iterator dir_it_end;
 	boost::filesystem::directory_iterator dir_it_cur;
 	DirWalkHandler dir_walk_handler;
+	CompleteHandler complete_handler;
 public:
 	typedef void result_type;
 
-	async_dir_walk( boost::asio::io_service & _io_service, boost::filesystem::path path, DirWalkHandler _dir_walk_handler )
-		: io_service( _io_service ), dir_it_cur(path), dir_walk_handler(_dir_walk_handler)
+	async_dir_walk( boost::asio::io_service & _io_service, boost::filesystem::path path, DirWalkHandler _dir_walk_handler, CompleteHandler _complete_handler)
+		: io_service( _io_service ), dir_it_cur(path), dir_walk_handler(_dir_walk_handler), complete_handler(_complete_handler)
 	{
 		io_service.post( boost::asio::detail::bind_handler( *this, boost::coro::coroutine() ) );
 	}
@@ -40,16 +41,22 @@ public:
 
 				_yield avloop_idle_post(io_service, boost::bind( *this, coro ) );
 			}
+
+			avloop_idle_post(io_service, complete_handler);
 		}
 	}
 
 };
 
+class DirWalkDumyHandler{
+public:
+	void operator()(){}
+};
 
-template<class DirWalkHandler>
-async_dir_walk<DirWalkHandler> make_async_dir_walk_op( boost::asio::io_service & io_service, boost::filesystem::path path, DirWalkHandler dir_walk_handler)
+template<class DirWalkHandler, class CompleteHandler>
+async_dir_walk<DirWalkHandler, CompleteHandler> make_async_dir_walk_op( boost::asio::io_service & io_service, boost::filesystem::path path, DirWalkHandler dir_walk_handler, CompleteHandler complete_handler)
 {
-	return async_dir_walk<DirWalkHandler>( io_service, path, dir_walk_handler);
+	return async_dir_walk<DirWalkHandler, CompleteHandler>( io_service, path, dir_walk_handler, complete_handler);
 }
 
 
@@ -58,7 +65,14 @@ async_dir_walk<DirWalkHandler> make_async_dir_walk_op( boost::asio::io_service &
 template<class DirWalkHandler>
 void async_dir_walk(boost::asio::io_service & io_service, boost::filesystem::path path, DirWalkHandler dir_walk_handler)
 {
-	detail::make_async_dir_walk_op(io_service, path,  dir_walk_handler);
+	detail::make_async_dir_walk_op(io_service, path,  dir_walk_handler, detail::DirWalkDumyHandler());
+}
+
+
+template<class DirWalkHandler, class CompleteHandler>
+void async_dir_walk(boost::asio::io_service & io_service, boost::filesystem::path path, DirWalkHandler dir_walk_handler, CompleteHandler complete_handler)
+{
+	detail::make_async_dir_walk_op(io_service, path,  dir_walk_handler, complete_handler);
 }
 
 }
