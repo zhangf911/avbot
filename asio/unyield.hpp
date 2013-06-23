6048ff5e03f5a453ab385c0bcc6bb6612ef136f0
kiki <kiki.caicai@gmail.com>
2013-06-23 14:29:54 +0800

    boost 1.54 has asio.coroutine, prepare for that

--- asio/unyield.hpp
+++ asio/unyield.hpp
@@ -2,7 +2,7 @@
 // unyield.hpp
 // ~~~~~~~~~~~
 //
-// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -12,10 +12,10 @@
 # undef reenter
 #endif
 
-#ifdef _yield
-# undef _yield
+#ifdef yield
+# undef yield
 #endif
 
-#ifdef _fork
-# undef _fork
+#ifdef fork
+# undef fork
 #endif
