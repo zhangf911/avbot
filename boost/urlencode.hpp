88179fbfc8bceccddf5743e70515fa1dcc824059
microcai <microcaicai@gmail.com>
2013-07-04 18:10:00 +0800

    boost urlencode

--- boost/urlencode.hpp
+++ boost/urlencode.hpp
@@ -1,19 +1,28 @@
-/**
- * @file   test.c
- * @author mathslinux <riegamaths@gmail.com>
- * @date   Thu May 24 22:56:52 2012
+
+
+/*
+ * Copyright (C) 2013  微蔡 <microcai@fedoraproject.org>
  *
- * @brief  The Encode and Decode helper is based on
- * code where i download from http://www.geekhideout.com/urlcode.shtml
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
-#include <ctype.h>
-#include <string>
-#include <algorithm>
+#pragma once
 
-#include "../urlencode.hpp"
+namespace boost{
+namespace iterator{
+
 template<class BaseIterator>
 struct url_encode_iterator
 {
@@ -144,12 +153,12 @@ public:
 	}
 };
 
-std::string url_encode(const std::string str)
-{
-	return std::string(url_encode_iterator<std::string::const_iterator>(str.begin()),
-		url_encode_iterator<std::string::const_iterator>(str.end()) );
 }
 
+std::string url_encode(const std::string str)
+{
+	return std::string(iterator::url_encode_iterator<std::string::const_iterator>(str.begin()),
+		iterator::url_encode_iterator<std::string::const_iterator>(str.end()) );
+}
 
-
-// kate: indent-mode cstyle; indent-width 4; replace-tabs off; tab-width 4;
+}
