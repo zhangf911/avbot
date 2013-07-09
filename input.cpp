3736468f204d26ebf6216573f0b27a83a807ab20
microcai <microcaicai@gmail.com>
2013-07-09 15:23:31 +0800

    fix build on windows: incorrectly signals2 useage

--- input.cpp
+++ input.cpp
@@ -11,23 +11,6 @@ static boost::signals2::signal< void(std::string)> input_got_one_line;
 
 #ifdef BOOST_ASIO_HAS_POSIX_STREAM_DESCRIPTOR
 
-static void inputread(const boost::system::error_code &, std::size_t,
-					  boost::shared_ptr<boost::asio::posix::stream_descriptor>,
-					  boost::shared_ptr<boost::asio::streambuf>  );
-#else
-// workarround windows that can use posix stream for stdin
-static void input_thread(boost::asio::io_service & io_service)
-{
-	while ( !boost::this_thread::interruption_requested() && !std::cin.eof()){
-		std::string line;
-		std::getline(std::cin, line);
-		io_service.post(boost::bind(input_got_one_line, ansi_utf8(line)));
-	}
-}
-#endif
-
-#ifdef BOOST_ASIO_HAS_POSIX_STREAM_DESCRIPTOR
-
 static void inputread(const boost::system::error_code & ec, std::size_t length,
 					  boost::shared_ptr<boost::asio::posix::stream_descriptor> stdin,
 					  boost::shared_ptr<boost::asio::streambuf> inputbuffer)
@@ -39,6 +22,16 @@ static void inputread(const boost::system::error_code & ec, std::size_t length,
 	input_got_one_line(line);
 
 	boost::asio::async_read_until(*stdin, *inputbuffer, '\n', boost::bind(inputread , _1,_2, stdin, inputbuffer));
+}
+#else
+// workarround windows that can use posix stream for stdin
+static void input_thread(boost::asio::io_service & io_service)
+{
+	while ( !boost::this_thread::interruption_requested() && !std::cin.eof()){
+		std::string line;
+		std::getline(std::cin, line);
+		input_got_one_line(ansi_utf8(line));
+	}
 }
 #endif
 
