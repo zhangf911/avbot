d2d43c754e3762d220ad77e3ac9bcb99806a6d7b
microcai <microcaicai@gmail.com>
2013-08-09 14:29:50 +0800

    code style for input.cpp

--- input.cpp
+++ input.cpp
@@ -12,8 +12,8 @@ static boost::signals2::signal< void(std::string)> input_got_one_line;
 #ifdef BOOST_ASIO_HAS_POSIX_STREAM_DESCRIPTOR
 
 static void inputread(const boost::system::error_code & ec, std::size_t length,
-					  boost::shared_ptr<boost::asio::posix::stream_descriptor> stdin,
-					  boost::shared_ptr<boost::asio::streambuf> inputbuffer)
+	boost::shared_ptr<boost::asio::posix::stream_descriptor> stdin,
+	boost::shared_ptr<boost::asio::streambuf> inputbuffer)
 {
 	std::istream input(inputbuffer.get());
 	std::string line;
@@ -21,13 +21,17 @@ static void inputread(const boost::system::error_code & ec, std::size_t length,
 
 	input_got_one_line(line);
 
-	boost::asio::async_read_until(*stdin, *inputbuffer, '\n', boost::bind(inputread , _1,_2, stdin, inputbuffer));
+	boost::asio::async_read_until(
+		*stdin, *inputbuffer, '\n',
+		boost::bind(inputread , _1,_2, stdin, inputbuffer)
+	);
 }
 #else
 // workarround windows that can use posix stream for stdin
 static void input_thread(boost::asio::io_service & io_service)
 {
-	while ( !boost::this_thread::interruption_requested() && !std::cin.eof()){
+	while (!boost::this_thread::interruption_requested() && !std::cin.eof())
+	{
 		std::string line;
 		std::getline(std::cin, line);
 		input_got_one_line(ansi_utf8(line));
@@ -38,11 +42,20 @@ static void input_thread(boost::asio::io_service & io_service)
 void start_stdinput(boost::asio::io_service & io_service)
 {
 #ifdef BOOST_ASIO_HAS_POSIX_STREAM_DESCRIPTOR
-		boost::shared_ptr<boost::asio::posix::stream_descriptor> stdin( new boost::asio::posix::stream_descriptor( io_service, 0 ) );
-		boost::shared_ptr<boost::asio::streambuf> inputbuffer( new boost::asio::streambuf );
-		boost::asio::async_read_until( *stdin, *inputbuffer, '\n', boost::bind( inputread , _1, _2, stdin, inputbuffer ) );
+		boost::shared_ptr<boost::asio::posix::stream_descriptor> stdin(
+			new boost::asio::posix::stream_descriptor( io_service, 0 )
+		);
+		boost::shared_ptr<boost::asio::streambuf> inputbuffer(
+			new boost::asio::streambuf
+		);
+		boost::asio::async_read_until(
+			*stdin, *inputbuffer, '\n',
+			boost::bind(inputread , _1, _2, stdin, inputbuffer)
+		);
 #else
-		boost::thread( boost::bind( input_thread, boost::ref( io_service ) ) );
+		boost::thread(
+			boost::bind(input_thread, boost::ref(io_service))
+		);
 #endif
 }
 
