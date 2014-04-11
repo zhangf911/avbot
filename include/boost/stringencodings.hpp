697cc0bdf892fb983c4bb2703a9730882d3e8e5d
microcai <microcaicai@gmail.com>
2014-04-12 00:12:40 +0800

    add local_encode_to_utf8

--- include/boost/stringencodings.hpp
+++ include/boost/stringencodings.hpp
@@ -45,3 +45,31 @@ inline std::string utf8_to_local_encode(const std::string & str)
 	return str;
 #endif
 }
+
+// convert to utf8 from console input aka native encoding
+// for linux ,  it does nothing (linux console always use utf8)
+// for windows, it convert from CP_ACP
+inline std::string local_encode_to_utf8(const std::string & str)
+{
+#if defined(_WIN32) || defined(_WIN64)
+
+	std::vector<WCHAR> wstr;
+
+	int required_buffer_size = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length(), NULL, 0);
+	wstr.resize(required_buffer_size);
+
+	wstr.resize(MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length(), &wstr[0], wstr.capacity()));
+
+	// 接着转换到UTF8
+	required_buffer_size = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], wstr.size(), NULL, 0, NULL, NULL);
+	std::vector<char> outstr;
+	outstr.resize(required_buffer_size);
+
+	int converted_size = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), wstr.size(), &outstr[0], required_buffer_size, NULL, NULL);
+	outstr.resize(converted_size);
+
+	return std::string(outstr.data(),outstr.size());
+#else
+	return str;
+#endif
+}
