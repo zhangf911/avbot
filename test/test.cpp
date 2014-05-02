3ab1f37c5a28c0f02f839643425af71848f7c744
microcai <microcaicai@gmail.com>
2014-05-02 14:20:07 +0800

    now we can decompress downloaded qqwry.rar
    next time, we need to add the ability to download that file on linux

--- test/test.cpp
+++ test/test.cpp
@@ -10,6 +10,15 @@
 #include <stdexcept>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <stdio.h>
+#if __cplusplus < 201103L
+# include <tr1/memory>
+# include <tr1/shared_ptr.h>
+namespace std{ using namespace std::tr1;}
+#else
+# include<memory>
+#endif
 
 #ifndef _WIN32
 #include <unistd.h>
@@ -22,13 +31,17 @@
 #include <wininet.h>
 #endif
 
-#include <memory>
-
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#include <stdio.h>
+#include <cstdio>
 #include <string.h>
+
+#ifdef HAVE_ZLIB
+#include <zlib.h>
+#else
+#include "../miniz.c"
+#endif
 
 #include "qqwry/ipdb.hpp"
 
@@ -47,6 +60,7 @@ struct copywritetag{
 
 #pragma pack()
 
+#define DEBUG
 
 #ifndef DEBUG
 #ifdef _WIN32
@@ -96,29 +110,32 @@ static std::string internetDownloadFile(std::string url)
 // common debug version that just load from file
 static std::string internetDownloadFile(std::string url)
 {
-	std::ifstream f;
+	std::shared_ptr<FILE> f;
+
 	if (url == "http://update.cz88.net/ip/copywrite.rar")
 	{
-		f.open("copywrite.rar", std::ios::binary);
+		f.reset(std::fopen("copywrite.rar", "rb"), std::fclose);
 	}
 	else if (url == "http://update.cz88.net/ip/qqwry.rar")
 	{
 		// 打开 qqwry.rar 吧
-		f.open("qqwry.rar", std::ios::binary);
+		f.reset(std::fopen("qqwry.rar", "rb"), std::fclose);
 	}
 	else
 		return "";
 
 	std::string ret;
-	ret.resize(819200);
- 	ret.resize( f.readsome(&ret[0], 819200) );
+	ret.resize(8192*1024);
+	ret.resize(std::fread(&ret[0], 1, ret.size(), f.get()));
+	//f.read(&ret[0], 8192);
+
 	return ret;
 }
 #endif
 
+#if defined(_WIN32) || defined(_WIN64)
 inline std::string wstring2string(std::wstring wstr)
 {
-#if defined(_WIN32) || defined(_WIN64)
 
 	// 接着转换到本地编码
 
@@ -130,34 +147,17 @@ inline std::string wstring2string(std::wstring wstr)
 	outstr.resize(converted_size);
 
 	return std::string(outstr.data(), outstr.size());
-#else
+//#else
 	// TODO, use with iconv
-	return str;
-#endif
+	//return str;
 }
+#endif
 
-
-// search for QQWry.Dat
-// first, look for ./QQWry.Dat
-// then look for /var/lib/QQWry.Dat
-// then look for $EXEPATH/QQWry.Dat
 #ifndef _WIN32
-std::string search_qqwrydat(const std::string exefile)
-{
-	if( access("QQWry.Dat", O_RDONLY) == 0){
-		return  "QQWry.Dat";
-	}
 
-	if ( access("/var/lib//QQWry.Dat", O_RDONLY) == 0){
-		return "/var/lib//QQWry.Dat";
-	}
-	// 找 exe 的位置
-	if ( exefile.find_last_of("/") != std::string::npos){
-		std::string ipfile = exefile.substr(0, exefile.find_last_of("/")+1);
-		ipfile += "QQWry.Dat";
-		return ipfile;
-	}
-	throw std::runtime_error("QQWry.Dat database not found");
+static bool check_exist(std::string filename)
+{
+	return access(filename.c_str(), O_RDONLY) ==  0;
 }
 #else
 static bool check_exist(std::string filename)
@@ -173,58 +173,103 @@ static bool check_exist(std::string filename)
 	dd.Invoke1(CComBSTR("FileExists"), &arg, &ret);
 	return ret.boolVal!=0;
 }
+#endif
 
-std::string get_parent_path(std::string path)
+// search for QQWry.Dat
+// first, look for ./QQWry.Dat
+// then look for /var/lib/QQWry.Dat
+// then look for $EXEPATH/QQWry.Dat
+std::string search_qqwrydat(const std::string exepath)
 {
-	CLSID clsid;
-	CLSIDFromProgID(CComBSTR("Scripting.FileSystemObject"), &clsid);
-	void *p;
-	CoCreateInstance(clsid, 0, CLSCTX_INPROC_SERVER, __uuidof(IDispatch), &p);
-	CComPtr<IDispatch> disp(static_cast<IDispatch*>(p));
-	CComDispatchDriver dd(disp);
-	CComVariant arg(path.c_str());
-	CComVariant ret("");
-	dd.Invoke1(CComBSTR("GetParentFolderName"), &arg, &ret);
-	return wstring2string(ret.bstrVal);
+	if (check_exist("QQWry.Dat"))
+	{
+		return  "QQWry.Dat";
+	}
+
+	if (check_exist("/var/lib/QQWry.Dat"))
+	{
+		return "/var/lib/QQWry.Dat";
+	}
+
+	// 找 exe 的位置
+	if (check_exist(exepath + "QQWry.Dat"))
+	{
+		return exepath + "QQWry.Dat";
+	}
+	//	throw std::runtime_error("QQWry.Dat database not found");
+
+	std::cout << "qqwry.dat 文件没找到，下载中......" << std::endl;
+
+	// 下载 copywrite.rar
+	std::string copywrite = internetDownloadFile("http://update.cz88.net/ip/copywrite.rar");
+	// 获取解压密钥 key
+	uint32_t key = QQWry::detail::to_hostending(reinterpret_cast<const copywritetag*>(copywrite.data())->key);
+//	uint32_t l = QQWry::detail::to_hostending(reinterpret_cast<const copywritetag*>(copywrite.data())->key);
+	std::string link = reinterpret_cast<const copywritetag*>(copywrite.data())->link;
+	// 下载 qqwry.rar
+	std::string qqwrydat = internetDownloadFile("http://update.cz88.net/ip/qqwry.rar");
+
+	std::vector<uint8_t> qqwrydata;
+
+	qqwrydata.resize(qqwrydat.size());
+	memcpy(&qqwrydata[0], qqwrydat.data(), qqwrydat.size());
+
+	// 解密
+	for (int i = 0; i<0x200; i++)
+	{
+		key *= 0x805;
+		key++;
+		key &=  0xFF;
+		uint32_t v = reinterpret_cast<const uint8_t*>(qqwrydat.data())[i] ^ key;
+
+		qqwrydat[i] = v;
+	}
+
+	// 解压
+	std::vector<uint8_t> deflated;
+	deflated.resize(20*1024*1024);
+	uLongf deflated_size = deflated.size();
+	uncompress(&deflated[0], &deflated_size, (const Bytef*) qqwrydat.data(), qqwrydat.size());
+	deflated.resize(deflated_size);
+	// 解压 qqwry.rar 为 qqwry.dat
+	std::ofstream ofile("QQWry.Dat", std::ios::binary);
+	ofile.write((const char*) deflated.data(), deflated.size());
+	ofile.close();
+
+	if (check_exist("QQWry.Dat"))
+		return "QQWry.Dat";
+throw  std::runtime_error("not found");
 }
 
-std::string search_qqwrydat()
+#ifdef _WIN32
+std::string  get_exe_dir(std::string argv0)
 {
 	std::vector<char> Filename;
 	Filename.resize(_MAX_FNAME);
 
 	GetModuleFileName(NULL, &Filename[0], _MAX_FNAME);
 
-	std::string exepath = get_parent_path( Filename.data() );
-
-	// 下载 copywrite.rar
-	std::string copywrite = internetDownloadFile("http://update.cz88.net/ip/copywrite.rar");
-	// 获取解压密钥 key
-	uint32_t key = ntohl(reinterpret_cast<const copywritetag*>(copywrite.data())->key);
-	std::string link = reinterpret_cast<const copywritetag*>(copywrite.data())->link;
-	// 下载 qqwry.rar
-	std::string qqwrydat = internetDownloadFile("http://update.cz88.net/ip/qqwry.rar");
-
-	for (int i = 0; i<0x200; i++)
-	{
-		key *= 0x805;
-		key++;
-		key &=  0xFF;
-		qqwrydat[i] ^= key;
-	}
-
-	// 解压 qqwry.rar 为 qqwry.dat
-	std::ofstream ofile("qqwry.rar", std::ios::binary);
-	ofile.write(qqwrydat.data(), qqwrydat.size());
-	ofile.close();
-
-	if (check_exist("QQWry.Dat"))
-		return "QQWry.Dat";
-
-	if (check_exist(exepath + "\\" + "QQWry.Dat"))
-		return exepath + "\\" + "QQWry.Dat";
-
-	throw  std::runtime_error("not found");
+	CLSID clsid;
+	CLSIDFromProgID(CComBSTR("Scripting.FileSystemObject"), &clsid);
+	void *p;
+	CoCreateInstance(clsid, 0, CLSCTX_INPROC_SERVER, __uuidof(IDispatch), &p);
+	CComPtr<IDispatch> disp(static_cast<IDispatch*>(p));
+	CComDispatchDriver dd(disp);
+	CComVariant arg(Filename.data());
+	CComVariant ret("");
+	dd.Invoke1(CComBSTR("GetParentFolderName"), &arg, &ret);
+	return wstring2string(ret.bstrVal);
+}
+#else
+std::string  get_exe_dir(std::string argv0)
+{
+	std::string exefile;
+	char* res =  realpath(argv0.c_str(),  NULL);
+	exefile = res;
+	free(res);
+	//　处理到　 / 位置
+	exefile.resize(exefile.find_last_of('/'));
+	return exefile;
 }
 #endif
 
@@ -233,11 +278,9 @@ int main(int argc,char * argv[])
 #ifdef _WIN32
 	CoInitialize(NULL);
 #endif
-#ifdef _WIN32
-	std::string ipfile = search_qqwrydat();
-#else
-	std::string ipfile = search_qqwrydat(argv[0]);
-#endif
+
+	std::string ipfile = search_qqwrydat(get_exe_dir(argv[0]));
+
 	QQWry::ipdb iplook(ipfile.c_str());
 
 	//使用 CIPLocation::GetIPLocation 获得对应 ip 地址的地区表示.
