c9e6999b05e2f30343691f5943dabc379486c1a4
jack <jack.wgm@gmail.com>
2014-05-17 14:23:07 +0800

    风格调整

--- avboost/include/boost/avloop.hpp
+++ avboost/include/boost/avloop.hpp
@@ -3,34 +3,37 @@
 #include <boost/asio.hpp>
 #include <boost/function/function0.hpp>
 #include <list>
-namespace detail{
+
+namespace detail {
+
 class IdleService
 	: public boost::asio::detail::service_base<IdleService>
 {
-	virtual void shutdown_service(){
-
-	}
+	virtual void shutdown_service()
+	{}
 
 	std::list<boost::function0<void> >	op_queue;
 
 public:
 
-	IdleService( boost::asio::io_service& owner )
-	  : boost::asio::detail::service_base<IdleService>(owner){}
+	IdleService(boost::asio::io_service& owner)
+		: boost::asio::detail::service_base<IdleService>(owner)
+	{}
 
-	bool has_idle() const {
+	bool has_idle() const
+	{
 		return !op_queue.empty();
 	}
 
 	template<class Handler>
-	void post(Handler handler){
+	void post(Handler handler)
+	{
 		op_queue.push_back(handler);
 	}
 
 	void poll_one()
 	{
-		//op_queue;
-		get_io_service().dispatch( op_queue.front() );
+		get_io_service().dispatch(op_queue.front());
 		op_queue.pop_front();
 	}
 };
@@ -49,7 +52,10 @@ class Win32MsgLoopService
 
 public:
 	Win32MsgLoopService(boost::asio::io_service& owner)
-		: boost::asio::detail::service_base<Win32MsgLoopService>(owner){ m_is_stoped = false; }
+		: boost::asio::detail::service_base<Win32MsgLoopService>(owner)
+	{
+		m_is_stoped = false;
+	}
 
 	bool has_message() const
 	{
@@ -73,37 +79,37 @@ public:
 		}
 	}
 };
+
 #endif
-}
+
+} // namespace detail
 
 template<class Handler>
-void avloop_idle_post(boost::asio::io_service & io_service, Handler handler)
+void avloop_idle_post(boost::asio::io_service& io_service, Handler handler)
 {
 	using namespace ::detail;
 	if (!boost::asio::has_service<IdleService>(io_service))
-	{
 		boost::asio::add_service(io_service, new IdleService(io_service));
-	}
 
 	boost::asio::use_service<IdleService>(io_service).post(handler);
 }
 
-
-static inline void avloop_run(boost::asio::io_service & io_service)
+static inline void avloop_run(boost::asio::io_service& io_service)
 {
 	using namespace ::detail;
 	boost::asio::io_service::work work(io_service);
 	if (!boost::asio::has_service<IdleService>(io_service))
-	{
 		boost::asio::add_service(io_service, new IdleService(io_service));
-	}
 
 	while (!io_service.stopped())
 	{
-		if(!boost::asio::use_service<IdleService>(io_service).has_idle()){
+		if(!boost::asio::use_service<IdleService>(io_service).has_idle())
+		{
 			io_service.run_one();
-		}else{
-			while (io_service.poll()){};
+		}
+		else
+		{
+			while (io_service.poll());
 			// 执行 idle handler!
 			boost::asio::use_service<IdleService>(io_service).poll_one();
 		}
@@ -111,36 +117,35 @@ static inline void avloop_run(boost::asio::io_service & io_service)
 }
 
 #ifdef _WIN32
-// MsgWaitForMultipleObjectsEx 集成进去
-static inline void avloop_run_gui(boost::asio::io_service & io_service)
+// MsgWaitForMultipleObjectsEx 集成进去.
+static inline void avloop_run_gui(boost::asio::io_service& io_service)
 {
 	using namespace ::detail;
+
 	boost::asio::io_service::work work(io_service);
+
 	if (!boost::asio::has_service<IdleService>(io_service))
-	{
 		boost::asio::add_service(io_service, new IdleService(io_service));
-	}
+
 	if (!boost::asio::has_service<Win32MsgLoopService>(io_service))
-	{
 		boost::asio::add_service(io_service, new Win32MsgLoopService(io_service));
-	}
 
 	while (!io_service.stopped())
 	{
-		// 首先处理 asio 的消息
+		// 首先处理 asio 的消息.
 		while (io_service.poll())
 		{
-			// 然后执行 gui 循环，看有没有 gui 事件
+			// 然后执行 gui 循环，看有没有 gui 事件.
 			if (boost::asio::use_service<Win32MsgLoopService>(io_service).has_message())
 			{
-				// 执行以下
+				// 执行以下.
 				boost::asio::use_service<Win32MsgLoopService>(io_service).poll_one();
 			}
 		}
-		// 然后执行 gui 循环，看有没有 gui 事件
+		// 然后执行 gui 循环，看有没有 gui 事件.
 		while(boost::asio::use_service<Win32MsgLoopService>(io_service).has_message())
 		{
-			// 执行以下
+			// 执行以下.
 			boost::asio::use_service<Win32MsgLoopService>(io_service).poll_one();
 		}
 
@@ -148,14 +153,13 @@ static inline void avloop_run_gui(boost::asio::io_service & io_service)
 		if (boost::asio::use_service<IdleService>(io_service).has_idle())
 			boost::asio::use_service<IdleService>(io_service).poll_one();
 
-		// 都没有事件了，执行 一次 1ms 的超时等待
-
+		// 都没有事件了，执行 一次 1ms 的超时等待.
 		auto ret = MsgWaitForMultipleObjectsEx(0, nullptr, 1, QS_ALLEVENTS, MWMO_WAITALL|MWMO_ALERTABLE | MWMO_INPUTAVAILABLE);
-		// 可能是有 gui 消息了， 呵呵， 从头来吧
+		// 可能是有 gui 消息了， 呵呵， 从头来吧.
 	}
 }
 #else
-static inline void avloop_run_gui(boost::asio::io_service & io_service)
+static inline void avloop_run_gui(boost::asio::io_service& io_service)
 {
 	return avloop_run(io_service);
 }
