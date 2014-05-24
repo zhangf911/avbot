4b1ce0819a0b5439e25c37778448b381d0d6c067
microcai <microcaicai@gmail.com>
2014-05-24 18:48:44 +0800

    move lots of things out of Cmakefile.lists use sub cmake file module

---
+++ tweakvcflasgs.cmake
@@ -0,0 +1,32 @@
+if(MSVC)
+
+set(CompilerFlags
+        CMAKE_CXX_FLAGS
+        CMAKE_CXX_FLAGS_DEBUG
+        CMAKE_CXX_FLAGS_RELEASE
+        CMAKE_C_FLAGS
+        CMAKE_C_FLAGS_DEBUG
+        CMAKE_C_FLAGS_RELEASE
+		CMAKE_EXE_LINKER_FLAGS
+		CMAKE_EXE_LINKER_FLAGS_DEBUG
+		CMAKE_EXE_LINKER_FLAGS_RELEASE
+        )
+
+foreach(CompilerFlag ${CompilerFlags})
+  string(REPLACE "/MDd" "/MTd" ${CompilerFlag} "${${CompilerFlag}}")
+endforeach()
+
+foreach(CompilerFlag ${CompilerFlags})
+  string(REPLACE "/W3" "/W1" ${CompilerFlag} "${${CompilerFlag}}")
+endforeach()
+
+foreach(CompilerFlag ${CompilerFlags})
+  string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
+endforeach()
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj /MP")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /DEBUG")
+
+#set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS}  /ignore:4099 /DEBUG /MTd /NODEFAULTLIB:libcmt.lib")
+endif()
