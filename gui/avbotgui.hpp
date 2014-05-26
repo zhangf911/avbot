d6e42f85a30d4e67c0c4657396a07204931a7b95
microcai <microcaicai@gmail.com>
2014-05-26 23:45:13 +0800

    rename show_dialog to setup_dialog

--- gui/avbotgui.hpp
+++ gui/avbotgui.hpp
@@ -11,6 +11,6 @@
 HWND async_input_box_get_input_with_image(boost::asio::io_service & io_service, std::string imagedata, boost::function<void(boost::system::error_code, std::string)> donecallback);
 #endif
 
-void show_dialog(std::string & qqnumber, std::string & qqpwd,
+void setup_dialog(std::string & qqnumber, std::string & qqpwd,
 	std::string & ircnick, std::string & ircroom, std::string & ircpwd,
 		std::string &xmppuser, std::string & xmppserver,std::string & xmpppwd, std::string & xmpproom, std::string &xmppnick);
