d4796fc91609a8d9ce24296566766e743433a2ec
Huang Yangqi <i@hyq.me>
2014-05-28 00:28:45 +0800

    add zmq publisher

---
+++ extension/zmqpublisher.cpp
@@ -0,0 +1,60 @@
+#include "zmqpublisher.hpp"
+#include "zmq.h"
+#include <boost/property_tree/json_parser.hpp>
+
+class ZmqPublisher
+{
+public:
+	ZmqPublisher()
+	{
+		ctx_ = zmq_ctx_new();
+		socket_ = zmq_socket(ctx_, ZMQ_PUB);
+		zmq_bind(socket_, "tcp://*:8123");
+	}
+
+	void send(const std::string& channel, const std::string& str)
+	{
+		zmq_send(socket_, channel.c_str(), channel.size(), ZMQ_SNDMORE);
+		zmq_send(socket_, str.c_str(), str.size(), 0);
+	}
+
+	virtual ~ZmqPublisher()
+	{
+		zmq_close(socket_);
+		zmq_ctx_term(ctx_);
+	}
+
+private:
+	void* ctx_;
+	void* socket_;
+};
+
+class ZmqPublisherClient
+	: virtual private ZmqPublisher
+{
+public:
+	ZmqPublisherClient(asio::io_service &io, std::string channel_name, boost::function<void (std::string)> sender)
+		: io_(io)
+		, channel_name_(channel_name)
+		, sender_(sender)
+	{
+
+	}
+
+	void operator()(boost::property_tree::ptree msg)
+	{
+		std::stringstream ss;
+		boost::property_tree::json_parser::write_json(ss, msg);
+		send(channel_name_, ss.str());
+	}
+
+private:
+	asio::io_service &io_;
+	std::string channel_name_;
+	boost::function<void (std::string)> sender_;
+};
+
+avbot_extension make_zmq_publisher(asio::io_service &io, std::string channel_name, boost::function<void (std::string)> sender)
+{
+	return avbot_extension(ZmqPublisherClient(io, channel_name, sender));
+}
---
+++ extension/zmqpublisher.hpp
@@ -0,0 +1,10 @@
+#include <boost/asio.hpp>
+#include <boost/property_tree/ptree.hpp>
+namespace asio = boost::asio;
+using boost::property_tree::ptree;
+#include <boost/random.hpp>
+#include <boost/function.hpp>
+
+#include "extension.hpp"
+
+avbot_extension make_zmq_publisher(asio::io_service& io, std::string channel_name, boost::function<void(std::string)> sender);
