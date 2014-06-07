6aa1ada734a77b5548210c7d3c63640940d85fdb
microcai <microcaicai@gmail.com>
2014-06-08 02:54:47 +0800

    MSVC 编译参数，这样就可以了其实。

--- tweakvcflasgs.cmake
+++ tweakvcflasgs.cmake
@@ -1,5 +1,5 @@
 if(MSVC)
-
+set (CMAKE_CONFIGURATION_TYPES "Release;Debug")
 set(CompilerFlags
         CMAKE_CXX_FLAGS
         CMAKE_CXX_FLAGS_DEBUG
@@ -9,29 +9,20 @@ set(CompilerFlags
         CMAKE_C_FLAGS_DEBUG
         CMAKE_C_FLAGS_RELEASE
 		CMAKE_C_FLAGS_MinSizeRel
-		CMAKE_EXE_LINKER_FLAGS
-		CMAKE_EXE_LINKER_FLAGS_DEBUG
-		CMAKE_EXE_LINKER_FLAGS_RELEASE
-		CMAKE_EXE_LINKER_FLAGS_MinSizeRel
    )
 
 foreach(CompilerFlag ${CompilerFlags})
-  string(REPLACE "/MDd" "/MTd" ${CompilerFlag} "${${CompilerFlag}}")
+  string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
 endforeach()
 
 foreach(CompilerFlag ${CompilerFlags})
   string(REPLACE "/W3" "/W1" ${CompilerFlag} "${${CompilerFlag}}")
 endforeach()
 
-foreach(CompilerFlag ${CompilerFlags})
-  string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
-endforeach()
-
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj /MP")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /DEBUG")
 
 #set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS}  /ignore:4099 /DEBUG /MTd /NODEFAULTLIB:libcmt.lib")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS}  /ignore:4099 /NODEFAULTLIB:libcmt.lib")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER}  /SAFESEH:NO")
 
 endif(MSVC)
